99db702045e97defd900d085c78cd393afeb1b93
Abdulc123 <149743322+Abdulc123@users.noreply.github.com>
2025-09-27 16:42:09 -0400

    fix: merged dev and universal color design feature branch

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
-import { Provider as PaperProvider, BottomNavigation } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
 import { ThemeProvider } from './theme/ThemeContext';
 
 import NavigationBar from './screens/NavigationBar';
 
 export default function App() {
   return (
-    // ThemeProvider makes the custom theme available to all children via useTheme()
     <ThemeProvider>
       <PaperProvider>
         <View style={styles.container}>
           <StatusBar style="auto" />
-          <BottomNavigation
-            navigationState={{ index, routes }}
-            onIndexChange={setIndex}
-            renderScene={renderScene}
-          />
+          {/* NavigationBar encapsulates the BottomNavigation state and scenes */}
+          <NavigationBar />
         </View>
       </PaperProvider>
     </ThemeProvider>
